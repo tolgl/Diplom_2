429be1766004a78ba87e9413799d004c7adac72b
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-18 15:29:25 +0300

    базовые методы

---
+++ clients/api_client.py
@@ -0,0 +1,27 @@
+import json
+import requests
+from config import BASE_URL
+
+
+class ApiClient:
+    def __init__(self):
+        self.base_url = BASE_URL
+
+    def post(self, path, payload, headers=None):
+        response_post = requests.post(url=self.base_url + path,
+                                      data=json.dumps(payload),
+                                      headers={"Content-type": "application/json", "Authorization": headers})
+        return response_post
+
+    def get(self, path, get_params=None):
+        response_get = requests.get(url=self.base_url + path,
+                                    params=get_params)
+
+        return response_get
+
+    def delete(self, path, get_params=None, headers=None):
+        response_delete = requests.get(url=self.base_url + path,
+                                       params=get_params,
+                                       headers={"Content-type": "application/json", "Authorization": headers})
+
+        return response_delete
