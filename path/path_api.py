1828ac75204060eb2069d82d5e431088e8d997d3
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-19 11:42:27 +0300

    добавлены эндпоинты логина пользователя, создания заказа и получение ингредиентов

--- path/path_api.py
+++ path/path_api.py
@@ -1,3 +1,6 @@
 class ApiPath:
     path_create_user = 'api/auth/register'
     path_get_user = 'api/auth/user'
+    path_login_user = 'api/auth/login'
+    path_order = 'api/orders'
+    path_get_ingredients = 'api/ingredients'
